2fab6f19c456d6dc2be85dcbc387a6b28866a396
RahulGupta <rahulrgd21@gmail.com>
2024-09-13 19:16:47 +0530

    initial commit

---
+++ src/main/java/com/launchcrew/launchcrew_app/LaunchcrewAppApplication.java
@@ -0,0 +1,13 @@
+package com.launchcrew.launchcrew_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchcrewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchcrewAppApplication.class, args);
+	}
+
+}
