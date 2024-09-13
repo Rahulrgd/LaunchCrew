2eda5c0b22306050b8bd431d7e8b670a32f8ec6a
RahulGupta <rahulrgd21@gmail.com>
2024-09-13 21:52:28 +0530

    Added mapstruct processor, and refactored UserMapper class.

--- src/main/java/com/launchcrew/launchcrew_app/User/UserMapper.java
+++ src/main/java/com/launchcrew/launchcrew_app/User/UserMapper.java
@@ -1,7 +1,5 @@
 package com.launchcrew.launchcrew_app.User;
 
-
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -10,13 +8,10 @@ import org.mapstruct.factory.Mappers;
 public interface UserMapper {
   UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-  @Mapping(target = "resumeDTO", source = "resumes")
   UserDTO toDTO(User user);
 
   @Mapping(target = "id", ignore = true)
-  @Mapping(target = "jobPosts", ignore = true)
   @Mapping(target = "password", ignore = true)
-  @Mapping(target = "resumes", ignore = true)
   // @Mapping(target = "authorities", ignore = true)
   User toEntity(UserDTO dto);
 }
